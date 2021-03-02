8f1ed67f66c1bfd4e824bfd2d2ed8ad3e5617f5b
kojiyonamine <kojiyonamine2@gmail.com>
2021-03-02 19:07:11 -0300

    Update arduino_noteira.ino
    resolvido bug para funcionar o corte do áudio quando não é colocado crédito e após acabar a música.

--- arduino_noteira.ino
+++ arduino_noteira.ino
@@ -1,22 +1,27 @@
 
-
 // DEFINIÇÕES
 #define pinNoteiro 2 // pino de interrupção do noteiro
 #define pinMoedas 3 // pino de interrupção das moedas
 #define pinSaida 8 // pino de saída que dá os créditos.
 #define inputAudio 7 // define o pino que recebe se há sinal de audio saindo do computador
 #define outputAudio 5 // Define o pino que dá o sinal de alto quando há saída de som do computador
-
 // DECLARAÇÃO DE VARIÁVEIS GLOBAIS
 int creditoInserido = 0;
 int contador_pulso = 0;
 int resto_contador = 0;
 int val=0;
-unsigned long espera_rodar_noteira=3000; //Espera 3 segundo após o início da noteira
+int analogPin = A0; // Define a leitura analógica
+int valor_analogico = 0;
+int logica_or_para_funcionamento_micro_A = 0;
+int logica_or_para_funcionamento_micro_B = 0;
+unsigned long espera_rodar_noteira=1000; //Espera 1 segundo após o início da noteira
 unsigned long tempoAtual = millis();
 unsigned long tempoInicial =0; // cronômetro utilizado no programa que faz a contagem das moedas
 unsigned long cronometro_audio = millis();
+unsigned long cronometro_microfone_credito = millis ();
 unsigned long espera_tempo_audio = 6000; // espera 6 segundos após acabar o sinal de áudio
+unsigned long espera_tempo_cronometro_microfone_credito = 15000;// tempo que o microfone fica aberto após colocar crédito
+
 ///////////////VARIÁVEIS
 void setup() {
   Serial.begin(9600); 
@@ -26,7 +31,7 @@ void setup() {
   pinMode ( inputAudio,INPUT);
   pinMode (outputAudio,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(pinNoteiro), leituraNoteiro, RISING);
-  attachInterrupt(digitalPinToInterrupt(pinMoedas), leituraMoedinhas, RISING);
+  attachInterrupt(digitalPinToInterrupt(pinMoedas), leituraMoedinhas, FALLING);
 }
 //*****************SETUP***********//
 
@@ -35,35 +40,73 @@ void leituraNoteiro()
   contador_pulso++;
   tempoInicial = millis();
   Serial.println("a interrupção funcionou! "); //APAGAR NO PROGRAMA EM PRODUÇÃO
-  Serial.println (tempoInicial); // APAGAR NO PROGRAMA EM PRODUÇÃO
+  //Serial.println (tempoInicial); // APAGAR NO PROGRAMA EM PRODUÇÃO
 }
 
 void leituraMoedinhas()
 {
+   static unsigned long last_interrupt_time = 0; //gambiarra para evitar erro de boucing
+   unsigned long interrupt_time = millis(); // continuação da gambi para o erro de bouncing
+   Serial.println ("entrou na interrupção");
+   if (interrupt_time - last_interrupt_time > 400)
+   {
    Serial.println("MOEDINHAS EM HIGH, é para estar em LOW"); //APAGAR NO PROGRAMA EM PRODUÇÃO
    contador_pulso++;
    tempoInicial = millis();
-   delay(700);
+   last_interrupt_time = interrupt_time;
+   }
+   
 }
 void loop (){
   //delay (5000);
-  val= digitalRead (inputAudio);
-  if ( val == HIGH)
-  {
+  valor_analogico=0;
+  valor_analogico = analogRead (analogPin);
+  Serial.println(valor_analogico);
+  
+ // val= digitalRead (inputAudio);
+  if ( valor_analogico > 100)
+     {
+     logica_or_para_funcionamento_micro_B = 1;
     digitalWrite (outputAudio, HIGH);
     cronometro_audio = millis();
-    Serial.println ("Saída audio em high");    
-  }
-  
-  if (millis() - cronometro_audio > espera_tempo_audio)
+   Serial.println ("Saída audio em high"); 
+   valor_analogico = 0;    
+      }
+      
+// Aqui vem a gambiarra que faz funcionar o microfone só quando coloca crédito, por 20 segundos
+if (millis () - cronometro_microfone_credito > espera_tempo_cronometro_microfone_credito)
+{
+   // digitalWrite (outputAudio, LOW);
+    logica_or_para_funcionamento_micro_A = 0;
+    Serial.println ("saída de áudio low por causa das moedas");
+    valor_analogico = 0;  
+}
+
+//Esse aqui é para deixar em 0V depois que acaba a música.  
+ if (millis() - cronometro_audio > espera_tempo_audio)
   {
-    digitalWrite (outputAudio, LOW);
+    
+    //digitalWrite (outputAudio, LOW);
+    logica_or_para_funcionamento_micro_B=0;
     Serial.println ("Saída audio em LOOOW");
+    valor_analogico = 0;  
+  }
+//aqui vem a lógica para funcionar o áudio tanto quando colocar crédito, quanto quando a música começar
+  if ((logica_or_para_funcionamento_micro_A == 1) || (logica_or_para_funcionamento_micro_B == 1))
+  {
+    digitalWrite(outputAudio,HIGH);
+    Serial.println( "estou aqui na lógica OR");
+  }
+  else
+  {
+  digitalWrite (outputAudio,LOW);
+  Serial.println("entrei no Else da lógica OR");
   }
   
+ 
   if(millis() - tempoInicial  > espera_rodar_noteira)
- { Serial.println ( tempoInicial);
-   Serial.println (millis());
+ { //Serial.println ( tempoInicial);
+   //Serial.println (millis());
    tempoInicial = millis();
   if (contador_pulso > 1)
   {
@@ -74,12 +117,18 @@ void loop (){
        for  ( int i = 0; i < contador_pulso/2; i = i+1) 
        {  
           digitalWrite(pinSaida, HIGH);
-          Serial.println("5 REAIS COMPUTADOS");
-          delay(500);
+          Serial.println("número de créditos impar computado");
+          delay(100);
           Serial.println("agora é para desligar o led");
           digitalWrite(pinSaida, LOW);
-          delay (500);
+          delay (100);
           resto_contador=0;
+          // *******************AQUI VEM A GAMBIARRA PARA FUNCIONAR O ÁUDIO DO PC via créditos
+          //digitalWrite (outputAudio, HIGH); precisa desabilitar, porque se não a lógica "or" não funciona
+          logica_or_para_funcionamento_micro_A = 1;
+          cronometro_microfone_credito = millis();
+         // cronometro_audio = millis(); // Precisa colocar isso aquim se não vai disparar o cronômetro do áudio
+          Serial.println ("aqui estou ligando o microfone, porque inseriram créditos impar");
        }
        Serial.println("CONTADOR DE PULSO =0");  
        contador_pulso=0;
@@ -91,13 +140,18 @@ void loop (){
         { 
          digitalWrite(pinSaida, HIGH);
          Serial.println("CREDITOU MOEDINHAS");
-         delay(500);
+         delay(100);
          Serial.println("agora é para desligar o led");
          digitalWrite(pinSaida, LOW);
-         delay (500);
+         delay (100);
          resto_contador = 0;
+         // **********************AQUI VEM A GAMBIARRA PARA FUNCIONAR O ÁUDIO DO PC via créditos
+          logica_or_para_funcionamento_micro_A = 1;
+          //digitalWrite (outputAudio, HIGH); precisa desabilitar, porque se não a lógica "or" não funciona
+          cronometro_microfone_credito = millis();
+          Serial.println ("aqui estou ligando o microfone, porque inseriram MOEDITAS");
        }
-     delay (500);
+     delay (100);
      contador_pulso = 1;
        Serial.println ("saiu do delay de 5 Segundos ");
       }
@@ -108,5 +162,3 @@ void loop (){
 
 
 }
-
-
